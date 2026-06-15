7e7c13fad175554c344ccbb246c2ced156472bab
aqilgulaliyev <gulaliyevaqil@gmail.com>
2026-06-15 23:19:21 +0400

    Initialization

---
+++ script.js
@@ -0,0 +1,16 @@
+// Sayt uğurla yükləndikdə konsola mesaj yazdırırıq (Yoxlamaq üçün)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portalın neon mühərriki işə düşdü... 🌌");
+    
+    // Gələcəkdə interaktiv animasiyaları idarə edəcəyimiz funksiyanın təməli:
+    initAnimations();
+});
+
+function initAnimations() {
+    const container = document.getElementById("animation-container");
+    
+    // Bura növbəti mərhələdə dinamik elementlər (məs. alqoritm vizuallaşdırmaları) gələcək.
+    if (container) {
+        // Hələlik boş saxlanılır (User request: dealing with this later)
+    }
+}
